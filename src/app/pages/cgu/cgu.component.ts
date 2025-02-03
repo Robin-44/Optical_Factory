30f5eaa824e4dd4a0cf7725d7e87ecedfd41a581
User <robin.mahe@epitech.digital>
2025-02-03 14:25:33 +0100

    Add CGU V1

---
+++ src/app/pages/cgu/cgu.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'cgu',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './cgu.component.html',
+  styleUrl: './cgu.component.css'
+})
+export class CGU {
+
+}
