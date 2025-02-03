f708749a5fc83b58c9c9d1216534deef53476e16
User <robin.mahe@epitech.digital>
2025-02-03 14:28:44 +0100

    Add mentions légales V1

---
+++ src/app/pages/mentions-legales/mentions-legales.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'mentions-legales',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './mentions-legales.component.html',
+  styleUrl: './mentions-legales.component.css'
+})
+export class MentionsLegales {
+
+}
