9d09f0e60965c838079d52b74bfd9da8448d82b0
User <robin.mahe@epitech.digital>
2025-02-03 14:27:01 +0100

    Add Cookies V1

---
+++ src/app/pages/cookies/cookies.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'cookies',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './cookies.component.html',
+  styleUrl: './cookies.component.css'
+})
+export class Cookies {
+
+}
